2f4a93b7a02c239cbe6385b51e62062638dddf7e
telecter <alexcidon9@gmail.com>
2025-05-17 14:49:49 -0400

    initial commit

---
+++ src/main/java/xyz/telecter/whatadvancements/WhatAdvancements.java
@@ -0,0 +1,20 @@
+package xyz.telecter.whatadvancements;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import xyz.telecter.whatadvancements.command.AdvancementInfoCommand;
+
+public class WhatAdvancements implements ModInitializer {
+	public static final String MOD_ID = "whatadvancements";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, env) -> {
+			AdvancementInfoCommand.register(dispatcher, registryAccess);
+		});
+	}
+}
---
+++ src/main/java/xyz/telecter/whatadvancements/command/AdvancementInfoCommand.java
@@ -0,0 +1,71 @@
+package xyz.telecter.whatadvancements.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementEntry;
+import net.minecraft.advancement.AdvancementProgress;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.argument.RegistryKeyArgumentType;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+
+public class AdvancementInfoCommand {
+	public static void register(CommandDispatcher<ServerCommandSource> dispatcher,
+			CommandRegistryAccess commandRegistryAccess) {
+		dispatcher.register(CommandManager.literal("advancementinfo")
+				.then(CommandManager
+						.argument("advancement",
+								RegistryKeyArgumentType
+										.registryKey(RegistryKey.ofRegistry(Identifier.ofVanilla("advancement"))))
+						.executes(ctx -> {
+							ServerPlayerEntity player = ctx.getSource().getPlayerOrThrow();
+							AdvancementEntry entry = RegistryKeyArgumentType.getAdvancementEntry(ctx, "advancement");
+							return execute(ctx.getSource(), player, entry);
+
+						})));
+	}
+
+	private static int execute(ServerCommandSource source, ServerPlayerEntity player, AdvancementEntry entry) {
+		AdvancementProgress progress = player.getAdvancementTracker().getProgress(entry);
+
+		MutableText feedback = player.getDisplayName().copy()
+				.append(" ")
+				.append(progress.isDone() ? "has already" : "has not yet")
+				.append(" ")
+				.append(Text.literal("completed the advancement "))
+				.append(entry.value().name().orElse(Advancement.getNameFromIdentity(entry)));
+		source.sendFeedback(() -> feedback, false);
+
+		if (progress.isAnyObtained()) {
+			source.sendMessage(Text.literal("The following criteria have been obtained:"));
+			MutableText obtainedFeedback = Text.empty();
+			for (String obtained : progress.getObtainedCriteria()) {
+				obtainedFeedback
+						.append(obtained)
+						.append("\n")
+						.formatted(Formatting.GREEN);
+			}
+			source.sendMessage(obtainedFeedback);
+		}
+		if (!progress.isDone()) {
+			source.sendMessage(Text.literal("The following criteria have been not been obtained:"));
+			MutableText unobtainedFeedback = Text.empty();
+			for (String unobtained : progress.getUnobtainedCriteria()) {
+				unobtainedFeedback
+						.append(unobtained)
+						.append("\n")
+						.formatted(Formatting.RED);
+			}
+			source.sendMessage(unobtainedFeedback);
+		}
+
+		return 1;
+	}
+}
